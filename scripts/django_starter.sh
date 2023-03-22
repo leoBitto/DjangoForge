522a30de145c445508ec545bb61ed1cec632e6bb
leoBitto <leonardobitto@gmail.com>
2023-03-22 18:28:06 +0100

    added sudo to django starter script

--- scripts/django_starter.sh
+++ scripts/django_starter.sh
@@ -1,15 +1,16 @@
 echo "Let's install django!"
-source ../env_dj/bin/activate
-pip install -r requirements.txt
+sudo source ../env_dj/bin/activate
+sudo pip install -r requirements.txt
 cd ..
 cd src
-python manage.py makemigrations
-python manage.py migrate
+sudo python manage.py makemigrations
+sudo python manage.py migrate
 echo "I have populated the database, now lets create the superuser"
 echo "follow the instructions:"
-python manage.py createsuperuser
+sudo python manage.py createsuperuser
 echo "now we gather the static files"
-python manage.py collectstatic
+sudo python manage.py collectstatic
 echo "I'm getting out of the environment"
-deactivate
+sudo deactivate
+cd ../scripts
 ./gunicorn_starter.sh
