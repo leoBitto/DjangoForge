072e2fc6c618b1d2c8d35c0049b60318639ed929
leoBitto <leonardobitto@gmail.com>
2023-10-25 15:35:40 +0200

    modified engine of db

--- src/base/settings.py
+++ src/base/settings.py
@@ -95,7 +95,7 @@ if DEBUG:
 else:
    DATABASES = {
        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': config('DATABASE_NAME'),
            'USER': config('DATABASE_USER'),
            'PASSWORD': config('DATABASE_PASS'),
