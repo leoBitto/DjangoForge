4bff6734bfd5c13d162f65e040797d092d9a580c
leoBitto <leonardobitto@gmail.com>
2024-04-01 22:26:54 +0200

    modifications to workflow

--- manager.sh
+++ manager.sh
@@ -3,22 +3,22 @@
 # Questa funzione elimina le immagini precedenti e avvia i container
 build_and_start_containers() {
     # Elimina i container e i volumi esistenti (opzionale)
-    sudo docker-compose -f docker-compose-dev.yml down -v --remove-orphans
+    sudo docker-compose -f docker-compose.dev.yml down -v --remove-orphans
 
     # Avvia i container Docker in background e ricrea le immagini se necessario
-    sudo docker-compose -f docker-compose-dev.yml up -d --build
+    sudo docker-compose -f docker-compose.dev.yml up -d --build
     echo "Immagini create"
 
     # Applica le migrazioni del database all'interno del container "web"
-    sudo docker-compose -f docker-compose-dev.yml exec web python manage.py migrate --noinput
+    sudo docker-compose -f docker-compose.dev.yml exec web python manage.py migrate --noinput
     echo "Migrazioni eseguite"
 
     # Raccoglie i file statici all'interno del container "web", cancellando quelli esistenti
-    sudo docker-compose -f docker-compose-dev.yml exec web python manage.py collectstatic --noinput --clear
+    sudo docker-compose -f docker-compose.dev.yml exec web python manage.py collectstatic --noinput --clear
     echo "File statici raccolti"
 
     # Crea un superuser con le credenziali dalle variabili d'ambiente
-    sudo docker-compose -f docker-compose-dev.yml exec web python manage.py createsuperuser 
+    sudo docker-compose -f docker-compose.dev.yml exec web python manage.py createsuperuser 
 
     echo "Superuser creato"
     echo "Server in esecuzione"
@@ -27,15 +27,15 @@ build_and_start_containers() {
 # Questa funzione avvia solo i container Docker
 start_containers() {
     # Avvia i container Docker in background 
-    sudo docker-compose -f docker-compose-dev.yml up -d 
+    sudo docker-compose -f docker-compose.dev.yml up -d 
     echo "Immagini create"
 
     # Applica le migrazioni del database all'interno del container "web"
-    sudo docker-compose -f docker-compose-dev.yml exec web python manage.py migrate --noinput
+    sudo docker-compose -f docker-compose.dev.yml exec web python manage.py migrate --noinput
     echo "Migrazioni eseguite"
 
     # Raccoglie i file statici all'interno del container "web", cancellando quelli esistenti
-    sudo docker-compose -f docker-compose-dev.yml exec web python manage.py collectstatic --noinput --clear
+    sudo docker-compose -f docker-compose.dev.yml exec web python manage.py collectstatic --noinput --clear
     echo "File statici raccolti"
 
     echo "Server in esecuzione"
@@ -44,7 +44,7 @@ start_containers() {
 # Questa funzione ferma i container Docker
 stop_containers() {
     # Ferma tutti i container precedenti
-    sudo docker-compose -f docker-compose-dev.yml down
+    sudo docker-compose -f docker-compose.dev.yml down
 
     echo "Server fermato"
 }
@@ -52,7 +52,7 @@ stop_containers() {
 # Questa funzione elimina tutti i container e i volumi
 destroy_containers() {
     # Elimina tutti i container e i volumi associati
-    sudo docker-compose -f docker-compose-dev.yml down -v --remove-orphans
+    sudo docker-compose -f docker-compose.dev.yml down -v --remove-orphans
 
     echo "Container e volumi eliminati"
 }
