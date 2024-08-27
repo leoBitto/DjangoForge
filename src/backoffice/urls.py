ce8800c24d81df6b179019963685313f284addb1
leoBitto <leonardobitto@gmail.com>
2024-08-27 19:48:23 +0200

    started report development

--- src/backoffice/urls.py
+++ src/backoffice/urls.py
@@ -6,10 +6,7 @@ app_name= 'backoffice'
 
 urlpatterns = [
     path('', dashboard, name="backoffice"),
-    path('select-report/', ReportTypeSelectionView.as_view(), name='select_report_type'),
-    path('monthly-snapshot/<int:year>/<int:month>/', MonthlySnapshotView.as_view(), name='monthly_snapshot'),
-    path('quality-control/<int:year>/<int:month>/', QualityControlView.as_view(), name='quality_control'),
-    path('temporal-aggregation/<str:aggregation_type>/<str:start_date>/<str:end_date>/', TemporalAggregationView.as_view(), name='temporal_aggregation'),
-
+    path('select-report/', select_report_type, name='select_report_type'),
+    path('report/<str:report_type>/', view_report, name='view_report'),
 
 ]
